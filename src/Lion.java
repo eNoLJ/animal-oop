66ac72b25c089e1aa450f2e34eac9071a43d51bb
eNoLJ <derosatam76@gmail.com>
2025-12-10 14:55:08 +0900

    feat: Lion 클래스 구현

---
+++ src/Lion.java
@@ -0,0 +1,44 @@
+public class Lion {
+
+    private String name;
+
+    private int hp;
+
+    private int attackPower;
+
+    public Lion(String name, int hp, int attackPower) {
+        this.name = name;
+        this.hp = hp;
+        this.attackPower = attackPower;
+    }
+
+    public void levelUp() {
+        hp += 10;
+        attackPower += 3;
+    }
+
+    public void takeDamage(int damage) {
+        hp -= damage;
+        if (hp <= 0) {
+            System.out.println("사자가 쓰러졌습니다!");
+            return;
+        }
+        System.out.println("사자가 상처를 입었습니다.");
+    }
+
+    public void attack(Lion lion) {
+        int damage = attackPower;
+
+        if (Math.random() < 0.3) {
+            damage *= 2;
+            System.out.println("강하게 물었습니다! (크리티컬 히트!)");
+        }
+
+        System.out.println(this.name + "이(가) " + lion.getName() + "을(를) 공격했습니다.");
+        lion.takeDamage(damage);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
