0c5fe1af4d76d2cde5a9da48d44c4de50a3c10a1
eNoLJ <derosatam76@gmail.com>
2025-12-10 17:10:02 +0900

    feat: 동물끼리의 전투 시뮬레이션 구현

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Animal lion = new Lion("사자", 100, 20);
+        Animal eagle = new Eagle("독수리", 80, 15, 80);
+        Animal turtle = new Turtle("거북이", 150, 10, 10);
+
+        lion.doAAction();
+        eagle.doEAction();
+        turtle.doRAction();
+
+        System.out.println("=== 전투 시작 ===");
+
+        lion.attack(eagle);
+        eagle.attack(turtle);
+        turtle.attack(lion);
+
+        ((Lion) lion).biteStrongly(eagle);
+        ((Eagle) eagle).diveAttack(turtle);
+        ((Turtle) turtle).hidingInTheShell(lion);
+
+        System.out.println("전투 종료");
+
+        System.out.println(lion);
+        System.out.println(eagle);
+        System.out.println(turtle);
     }
 }
